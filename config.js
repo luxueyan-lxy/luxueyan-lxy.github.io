dd3d6050d449c6f43832ef0ef0a3013c68dd6439
luxueyan-lxy <420501973@qq.com>
2026-05-12 14:40:16 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // window.API_BASE = 'http://localhost:8000';
-window.API_BASE = 'https://baker-licensed-accept-tuning.trycloudflare.com';
+// window.API_BASE = 'http://192.168.0.105:8000';
+window.API_BASE = 'https://nursery-query-personal-tuner.trycloudflare.com';
