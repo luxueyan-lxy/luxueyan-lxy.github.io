6910514976376a1dc2309c704d3a30baf535a82a
luxueyan-lxy <420501973@qq.com>
2026-05-13 17:19:35 +0800

    更新

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 // window.API_BASE = 'http://localhost:8000';
 // window.API_BASE = 'http://192.168.0.105:8000';
 window.API_BASE = 'https://nursery-query-personal-tuner.trycloudflare.com';
+
+window.OPEN_API = "https://openapi.zhihu.com";
+
+window.BASE_URL = "https://developer.zhihu.com";
+
+window.WEB_URL = "https://luxueyan-lxy.github.io/";
